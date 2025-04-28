eb0bddb1bb1d23aecfcf72455d7a9f587f42fe54
JudyT01 <168handmade@gmail.com>
2025-04-28 14:53:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import os
 import streamlit as st
+from pydantic import BaseModel, validator
 import base64
 from crewai import Crew, Process, Agent, Task
 from crewai_tools import ScrapeWebsiteTool, PDFSearchTool, FileReadTool
